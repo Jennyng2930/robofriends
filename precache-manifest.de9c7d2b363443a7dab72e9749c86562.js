008da333b88b3afbdd301e7a3effad140d31685d
System Administrator <root@jens-MacBook-Pro.local>
2020-06-15 17:28:22 +0800

    Updates

---
+++ precache-manifest.de9c7d2b363443a7dab72e9749c86562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78157ac53e6116287160e4b758b404c0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "092d4393b682e4de6bf9",
+    "url": "/robofriends/static/css/main.ae06be9d.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092d4393b682e4de6bf9",
+    "url": "/robofriends/static/js/main.2e06244d.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
